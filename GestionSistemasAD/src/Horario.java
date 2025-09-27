caaf89f8d073e134618715871929b788a565bcd1
ElTrompa <159024200+Trompa06@users.noreply.github.com>
2025-09-27 13:38:38 +0200

    Horario.java estructura

--- GestionSistemasAD/src/Horario.java
+++ GestionSistemasAD/src/Horario.java
@@ -1,2 +1,35 @@
 public class Horario {
+    private String diaSemana;
+    private String hora;
+    private String grupo; //Libre o el grupo asignado
+
+    public Horario(String grupo, String hora, String diaSemana) {
+        this.grupo = grupo;
+        this.hora = hora;
+        this.diaSemana = diaSemana;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
 }
